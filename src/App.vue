e61ca20ea95fd5d70fe5fc8b20d2587fb927d630
ZoyidjonNasretdinov <zoyidjonnasretdinovcoder@gmail.com>
2025-02-27 01:56:03 +0500

    first project

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <h1>Counter {{ counter }}</h1>
+    <div class="d-flex">
+      <button @click="onDecrement" class="btn btn-danger">-</button>
+      <button @click="onIncrement" class="btn btn-success mx-3">+</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter : 1 ,
+    }
+  },
+
+  methods: {
+    onIncrement() {
+      this.counter += 1
+    },
+
+    onDecrement() {
+      this.counter -= 1
+    }
+  },
+}
+</script>
+
+<style></style>
